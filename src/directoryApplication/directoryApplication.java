1f97642afdfe506c36984698d259e8fef8ee8a4d
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2018-12-28 00:51:52 +0530

    fiddling with java internally to understand

---
+++ src/directoryApplication/directoryApplication.java
@@ -0,0 +1,26 @@
+package directoryApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class directoryApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		peeking inside the javacode to understand things
+//		ArrayList<String> l= new ArrayList<>();
+//		l.add("nik");
+//		l.add("sid");
+//		l.add("don");
+//		
+//		System.out.println(l);
+////		Collections.sort(l);
+//		l.sort();
+//		System.out.println(l);
+//		System.out.println(l.contains("nik")?"found":"not found");
+//		l.remove("sid");
+//		System.out.println(l);
+
+	}
+
+}
