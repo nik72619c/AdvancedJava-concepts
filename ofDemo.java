90868aefae2476705d3a4a4db7338b1f0460a8cf
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-01-02 23:13:50 +0530

    understanding of in collections

---
+++ ofDemo.java
@@ -0,0 +1,13 @@
+package directoryApplication;
+
+import java.util.List;
+
+public class ofDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> l = List.of(10,20,30,40,50);
+
+	}
+
+}
