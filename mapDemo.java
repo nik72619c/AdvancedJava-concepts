a737faf348e2282664fc743e59cd0dc5449af75e
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-01-17 21:39:04 +0530

    iterating entries

--- mapDemo.java
+++ mapDemo.java
@@ -1,19 +1,36 @@
 package Collections;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class mapDemo {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Map< String,Integer> hm =  
                  new HashMap< String,Integer>(); 
-hm.put("a", new Integer(100)); 
-hm.put("b", new Integer(200)); 
-hm.put("c", new Integer(300)); 
-hm.put("d", new Integer(400)); 
+hm.put("a", 100); 
+hm.put("b", 200); 
+hm.put("c", 300); 
+hm.put("d", 400); 
 System.out.println(hm);
+
+Iterator<String> iterator = hm.keySet().iterator();
+while(iterator.hasNext()){
+  Object key   = iterator.next();
+  Object value = hm.get(key);
+}
+
+Set entries = hm.entrySet();
+Iterator itr = entries.iterator();
+while(iterator.hasNext()) {
+    Map.Entry entry = (Map.Entry) itr.next();
+    Object key = entry.getKey();
+    Object value = entry.getValue();
+}
 
 	}
 
