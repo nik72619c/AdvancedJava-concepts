de444edf9c88a65a7aa774cb981d7a6b5b937559
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-01-27 22:47:07 +0530

    first servlet

---
+++ Servlets/src/AddServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out=res.getWriter();
+		res.setContentType("text/html");
+		
+		int num1=Integer.parseInt(req.getParameter("num1"));
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		int k=num1+num2;
+		out.println("ans is "+k);
+		out.close();
+	}
+}
