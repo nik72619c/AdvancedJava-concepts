a751ca43f886a6d85c7716214db9486949bc4a8d
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-01-28 19:32:57 +0530

    Initialising servlets without servlet descriptor

--- Servlets/src/AddServlet.java
+++ Servlets/src/AddServlet.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
