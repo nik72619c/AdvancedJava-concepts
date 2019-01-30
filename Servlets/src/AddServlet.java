ee280020b1f60425ccc9d9f5ac4200d5b9d9e06b
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-01-30 15:40:32 +0530

    Studied RequestDispatcher

--- Servlets/src/AddServlet.java
+++ Servlets/src/AddServlet.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,14 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		PrintWriter out=res.getWriter();
 		res.setContentType("text/html");
 		
 		int num1=Integer.parseInt(req.getParameter("num1"));
 		int num2=Integer.parseInt(req.getParameter("num2"));
 		int k=num1+num2;
-		out.println("ans is "+k);
+		RequestDispatcher rd=req.getRequestDispatcher("/index.html");
+		rd.include(req, res);
+		out.println("<h1>ans is "+k+"</h1>");
 		out.close();
 	}
 }
